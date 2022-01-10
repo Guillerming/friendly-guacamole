86c3dee84a08ec6259db345bd534ab1be30c349e
Guillerming <hello@guillerming.es>
2022-01-10 22:07:39 +0100

    #6: Gulp scss adds sourcemaps!

--- gulpfile.js/tasks/scss.js
+++ gulpfile.js/tasks/scss.js
@@ -3,12 +3,15 @@ const { src, dest } = require('gulp');
 const config = require('../config');
 
 const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-dart-sass');
 
 function compile() {
     return src(config.cfg.path.dist + 'app/temp.styles.scss')
-        .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(sass(config.cfg.sass))
         .pipe(rename('styles.css'))
+        .pipe(sourcemaps.write('/maps'))
         .pipe(dest(config.cfg.path.dist + 'public/assets/css/'));
 }
 
