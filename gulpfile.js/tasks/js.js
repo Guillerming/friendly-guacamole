f8d9dbaa449d91363e11671479df736b396a0d2f
Guillerming <hello@guillerming.es>
2022-01-29 21:19:15 +0100

    #8: Minify user scripts

--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -1,5 +1,5 @@
 'use strict';
-const { src, dest } = require('gulp');
+const { src, dest, parallel } = require('gulp');
 const config = require('../config');
 
 const minify = require('gulp-uglify');
@@ -7,7 +7,7 @@ const concat = require('gulp-concat');
 
 let scriptsMap;
 
-function minify_js() {
+function concat_min_vendor() {
     scriptsMap = require('../../build/app/temp.scripts.json');
     return src(scriptsMap.vendor)
         .pipe(minify())
@@ -15,4 +15,32 @@ function minify_js() {
         .pipe(dest(config.cfg.path.dist + 'public/assets/js/'));
 }
 
-exports.run = minify_js;
+function minify_user_scripts() {
+    let cwd = __dirname;
+    cwd = cwd.split('/');
+    for ( let n = 0; n < cwd.length; n++ ) {
+        if ( cwd.pop() != 'gulpfile.js' ) {
+            continue;
+        } else {
+            break;
+        }
+    }
+    cwd = cwd.join('/');
+    // console.log(cwd);
+    // return src('*');
+    scriptsMap = require('../../build/app/temp.scripts.json');
+    return scriptsMap.user.forEach(( user ) => {
+        return user.files.forEach(( filename ) => {
+            filename = filename.replace( cwd + '/', '');
+            let path = filename.split('/');
+            filename = path[path.length - 1];
+            path.pop();
+            path = path.join('/') + '/';
+            return src(path + filename)
+                .pipe(minify())
+                .pipe(dest(path));
+        })
+    });
+}
+
+exports.run = parallel(concat_min_vendor, minify_user_scripts);
