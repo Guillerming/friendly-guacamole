bd1e8927b2770c6fd4206509293d98c04cac0697
Guillerming <hello@guillerming.es>
2022-01-29 21:41:02 +0100

    #8: Component scripts injector

--- src/_core/modules/ComponentsModule.php
+++ src/_core/modules/ComponentsModule.php
@@ -70,8 +70,14 @@
             if ( !isset($this->components_registry[$component_id]) ) {
                 return false;
             }
+            // Preparing component capsule (wrapper)
+            $component_wrapper  = $this->friendlyGuacamole->SETTINGS['wrapper']['prefix'];
+            $component_wrapper .= 'com-';
+            $component_wrapper .= substr(md5(rand(0,9).(new DateTime())->getTimestamp()),0,3);
+            // Get scripts
+            $component_scripts = $this->friendlyGuacamole->ScriptsModule->get($this->lib->convert_entity_id_to_wrapper_tagname($component_id), $component_wrapper);
             $output = '';
-            $output .= '<'.$this->lib->convert_entity_id_to_wrapper_tagname($component_id).'>';
+            $output .= '<'.$this->lib->convert_entity_id_to_wrapper_tagname($component_id).' '.$component_wrapper.'>';
             ob_start(null, 0, PHP_OUTPUT_HANDLER_FLUSHABLE | PHP_OUTPUT_HANDLER_REMOVABLE);
             global $friendlyGuacamole;
             for ( $n = 0; $n < count($this->components_registry[$component_id]['view']['templates']); $n++ ) {
@@ -79,8 +85,8 @@
                 $output .= ob_get_contents();
             }
             ob_end_clean();
-            // ob_end_flush();
-            $output .= '</'.$this->lib->convert_entity_id_to_wrapper_tagname($component_id).'>';
+            // Append layout tag wrapper
+            $output .= $component_scripts.'</'.$this->lib->convert_entity_id_to_wrapper_tagname($component_id).'>';
             return $output;
         }
 
